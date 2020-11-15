de64bca0bf4018066b8e698de8fadd6ecd06ed0c
adityavj <adiiitya08@gmail.com>
2020-11-15 23:56:24 +0100

    router for links

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import BarGraphs from "./components/graphs/BarGraphs";
 import HomePage from "./components/homepage/HomePage";
 import ButtonPages from "./components/maincomponents/buttons/ButtonPages";
-
+import { BrowserRouter as Router } from "react-dom";
 import MenuBar from "./components/menubar/menubar";
 
 function App() {
 	return (
 		<>
-			<MenuBar></MenuBar>
-			<ButtonPages></ButtonPages>
+			<Router>
+				<MenuBar></MenuBar>
+				<ButtonPages></ButtonPages>
 
-			<BarGraphs></BarGraphs>
+				<BarGraphs></BarGraphs>
 
-			<HomePage />
+				<HomePage />
+			</Router>
 		</>
 	);
 }
