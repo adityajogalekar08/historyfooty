a05afbbd87f37420f622c22374d0b3edf9f66253
adityavj <adiiitya08@gmail.com>
2020-11-16 01:04:43 +0100

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 import BarGraphs from "./components/graphs/BarGraphs";
 import HomePage from "./components/homepage/HomePage";
 import ButtonPages from "./components/maincomponents/buttons/ButtonPages";
-import { BrowserRouter as Router } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import MenuBar from "./components/menubar/menubar";
-
+import React from "react";
 function App() {
 	return (
 		<>
--- src/components/menubar/menubar.js
+++ src/components/menubar/menubar.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Navbar } from "react-bootstrap";
 import "./menubar.css";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../../components/globalStyles";
+import { lightTheme, darkTheme } from "../../components/Themes";
+
+import { FaRegMoon as Moon } from "react-icons/fa";
 function MenuBar() {
+	const [theme, setTheme] = useState("light");
+	const themeToggler = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	};
 	return (
-		<>
-			<Container fluid className="appheader">
-				<Navbar expand="lg" className="navbar">
-					<Navbar.Brand bsPrefix="title">Title</Navbar.Brand>
-				</Navbar>
-			</Container>
-		</>
+		<ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+			<>
+				<GlobalStyles />
+
+				<Container fluid className="appheader">
+					<Navbar expand="lg" bsPrefix="navbar">
+						<Navbar.Brand bsPrefix="title">Title</Navbar.Brand>
+						<Navbar.Toggle />
+						<Navbar.Collapse className="justify-content-end">
+							<Navbar.Text bsPrefix="title">
+								<button onClick={themeToggler} className="cssMode">
+									<Moon className="moon" />
+								</button>
+							</Navbar.Text>
+						</Navbar.Collapse>
+					</Navbar>
+				</Container>
+			</>
+		</ThemeProvider>
 	);
 }
 export default MenuBar;
