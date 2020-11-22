d2f1d14e62a008e2edffb1ace5926b30e868f512
adityavj <adiiitya08@gmail.com>
2020-11-22 04:22:45 +0100

    dashboard data

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,63 +3,61 @@ import { Col, Container, Row } from "react-bootstrap";
 import "./dashboard.css";
 import "../../App.css";
 import BarGraphs from "../graphs/BarGraphs";
+import data from "../assets/json/manutd.json";
 
-function dasboard() {
+const DataTable = ({ name, club, info, opened, capacity, avg, record }) => {
 	return (
 		<>
-			<div class="wrapper">
-				<section class="top-container">
-					<header class="showcase">
-						<h1>Your Web Presence</h1>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
-							ipsam! Nihil quo minima nulla atque!
-						</p>
+			<div className="wrapper">
+				<section className="top-container">
+					<header className="showcase">
+						<h1>{club}</h1>
+						<p>{info}</p>
 					</header>
-					<div class="top-box top-box-a">
-						<h4>Membership</h4>
-						<p class="price">$199/mo</p>
+					<div className="top-box top-box-a">
+						<h4>Opened in</h4>
+						<p className="price">{opened}</p>
 					</div>
-					<div class="top-box top-box-b">
-						<h4>Pro Membership</h4>
-						<p class="price">$299/mo</p>
+					<div className="top-box top-box-b">
+						<h4>Capacity</h4>
+						<p className="price">{capacity}</p>
 					</div>
-					<div class="top-box top-box-c">
-						<h4>Pro Membership</h4>
-						<p class="price">$299/mo</p>
+					<div className="top-box top-box-c">
+						<h4>Average Attendance</h4>
+						<p className="price">{avg}</p>
 					</div>
-					<div class="top-box top-box-d">
-						<h4>Pro Membership</h4>
-						<p class="price">$299/mo</p>
+					<div className="top-box top-box-d">
+						<h4>Record Attendance</h4>
+						<p className="price">{record}</p>
 					</div>
 				</section>
-				<section class="boxes">
-					<div class="box">
-						<i class="fas fa-chart-pie fa-4x"></i>
+				<section className="boxes">
+					<div className="box">
+						<i className="fas fa-chart-pie fa-4x"></i>
 						<h3>Analytics</h3>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
 							expedita?
 						</p>
 					</div>
-					<div class="box">
-						<i class="fas fa-globe fa-4x"></i>
+					<div className="box">
+						<i className="fas fa-globe fa-4x"></i>
 						<h3>Marketing</h3>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
 							expedita?
 						</p>
 					</div>
-					<div class="box">
-						<i class="fas fa-cog fa-4x"></i>
+					<div className="box">
+						<i className="fas fa-cog fa-4x"></i>
 						<h3>Development</h3>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
 							expedita?
 						</p>
 					</div>
-					<div class="box">
-						<i class="fas fa-users fa-4x"></i>
+					<div className="box">
+						<i className="fas fa-users fa-4x"></i>
 						<h3>Support</h3>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
@@ -70,13 +68,28 @@ function dasboard() {
 			</div>
 		</>
 	);
+};
+function dasboard() {
+	return data.map((i, key) => {
+		return (
+			<DataTable
+				name={i.name}
+				club={i.club}
+				info={i.info}
+				opened={i.opened}
+				capacity={i.capacity}
+				avg={i.avgAttendance}
+				first={i.recordAttn}
+			/>
+		);
+	});
 }
 export default dasboard;
 
 /*
-	<div class="wrapper">
+	<div className="wrapper">
 		<!-- Navigation -->
-		<nav class="main-nav">
+		<nav className="main-nav">
 		<ul>
 			<li>
 			<a href="#">Home</a>
@@ -94,61 +107,61 @@ export default dasboard;
 		</nav>
 
 		<!-- Top Container -->
-		<section class="top-container">
-		<header class="showcase">
+		<section className="top-container">
+		<header className="showcase">
 			<h1>Your Web Presence</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, ipsam! Nihil quo minima nulla atque!</p>
-			<a href="#" class="btn">Read More</a>
+			<a href="#" className="btn">Read More</a>
 		</header>
-		<div class="top-box top-box-a">
+		<div className="top-box top-box-a">
 			<h4>Membership</h4>
-			<p class="price">$199/mo</p>
-			<a href="" class="btn">Buy Now</a>
+			<p className="price">$199/mo</p>
+			<a href="" className="btn">Buy Now</a>
 		</div>
-		<div class="top-box top-box-b">
+		<div className="top-box top-box-b">
 			<h4>Pro Membership</h4>
-			<p class="price">$299/mo</p>
-			<a href="" class="btn">Buy Now</a>
+			<p className="price">$299/mo</p>
+			<a href="" className="btn">Buy Now</a>
 		</div>
 		</section>
 
 		<!-- Boxes Section -->
-		<section class="boxes">
-		<div class="box">
-			<i class="fas fa-chart-pie fa-4x"></i>
+		<section className="boxes">
+		<div className="box">
+			<i className="fas fa-chart-pie fa-4x"></i>
 			<h3>Analytics</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
 		</div>
-		<div class="box">
-			<i class="fas fa-globe fa-4x"></i>
+		<div className="box">
+			<i className="fas fa-globe fa-4x"></i>
 			<h3>Marketing</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
 		</div>
-		<div class="box">
-			<i class="fas fa-cog fa-4x"></i>
+		<div className="box">
+			<i className="fas fa-cog fa-4x"></i>
 			<h3>Development</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
 		</div>
-		<div class="box">
-			<i class="fas fa-users fa-4x"></i>
+		<div className="box">
+			<i className="fas fa-users fa-4x"></i>
 			<h3>Support</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
 		</div>
 		</section>
 
 		<!-- Info Section -->
-		<section class="info">
+		<section className="info">
 		<img src="https://image.ibb.co/j4Qz8x/pic1.jpg" alt="">
 		<div>
 			<h2>Your Business On The Web</h2>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae alias reiciendis deleniti possimus nemo non repellendus?
 			Quae atque vero modi quidem! Autem cupiditate fugit doloribus ad amet, asperiores provident commodi.</p>
-			<a href="#" class="btn">Learn More</a>
+			<a href="#" className="btn">Learn More</a>
 		</div>
 		</section>
 
 		<!-- Portfolio -->
-		<section class="portfolio">
+		<section className="portfolio">
 		<img src="https://source.unsplash.com/random/200x200" alt="">
 		<img src="https://source.unsplash.com/random/201x200" alt="">
 		<img src="https://source.unsplash.com/random/202x200" alt="">
